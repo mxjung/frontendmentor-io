61e6be03b87ca2143acaddfc1316f91b72750db2
Max Jung <mjung@Peters-MBP.socal.rr.com>
2020-12-30 23:34:15 -0800

    started todo app

---
+++ todo-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="attribution">
+      Challenge by
+      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+      Coded by <a href="#">Your Name Here</a>.
+    </div>
+  );
+}
+
+export default App;
